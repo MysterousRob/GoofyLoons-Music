49785dfddf97100767bc72b0b233c4de4f4c687a
MysterousRob <robertjohngasior@gmail.com>
2024-06-18 17:33:10 +0200

    Implement login functionality (currently non-functional)
    - Created login interface and backend logic
    - Address ongoing issues preventing successful login

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Nav1 } from "../../State/Nav";
+import LoginPopup from "../LogIn/LogInPopUp";
 
 function Header() {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  }
+
   return (
     <div className="relative w-full">
       <div className="top-0 left-0 bg-logo2 h-64 w-full bg-cover bg-center flex items-center p-[10px] relative">
         <div className="absolute bottom-5 left-4 bg-white rounded-3xl p-[10px]">
-          <h1 className="text-6xl font-bold text-black text-left">
+          <h1 
+          className="text-6xl font-bold text-black text-left cursor-pointer"
+          onClick={togglePopup}
+          >
             GoonyLoons
           </h1>
         </div> 
@@ -14,6 +24,7 @@ function Header() {
           <Nav1 />
         </div>
       </div>
+      <LoginPopup isOpen={isPopupOpen} togglePopup={togglePopup}/>
     </div>
   );
 }
