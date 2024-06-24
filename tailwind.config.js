fbcd036857b4a1ce15d2a42f64dd8490e19d681c
MysterousRob <robertjohngasior@gmail.com>
2024-06-25 01:48:35 +0200

    changed to relative path

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ export default {
     },
       backgroundImage: {
         logo: "url('src/assets/pictures/OIP.jpg')",
-        logo2: "url('C:/Users/rober/OneDrive/Desktop/Fixit-proj-music/src/assets/pictures/eye2.jpg')"
+        logo2: "url('src/assets/pictures/eye2.jpg')"
       }
     },
   },
